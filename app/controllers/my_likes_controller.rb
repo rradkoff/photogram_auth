0ed98581ef44e1c1b5dae9d847e02ee4a31fba5b
rradkoff <rradkoff@gmail.com>
2015-11-22 13:21:57 -0600

    code for hw 1-16

---
+++ app/controllers/my_likes_controller.rb
@@ -0,0 +1,2 @@
+class MyLikesController < ApplicationController
+
---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,62 @@
+class PhotosController < ApplicationController
+
+  before_action :curret_user_must_be_owner, :only => [:edit, :update, :destroy]
+
+  skip_before_action :authenticate_user!, :only => [:index]
+
+  def curret_user_must_be_owner
+    @photo = Photo.find(params[:id])
+    if @photo.user != current_user
+      redirect_to root_url, :alert => "Not authorized for that."
+    end
+  end
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new
+    @photo.caption = params[:caption]
+    @photo.image = params[:image]
+    @photo.user_id = params[:user_id]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    @photo.caption = params[:caption]
+    @photo.image = params[:image]
+    @photo.user_id = params[:user_id]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+      redirect_to "/photos", :notice => "Photo deleted."
+  end
+end
